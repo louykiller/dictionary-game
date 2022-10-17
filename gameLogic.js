6317405a6dad0174d183d261ec3a18da8ec4e5cb
Lourenço Carvalho <lourencoccvc@gmail.com>
2022-10-17 16:10:47 +0100

    Deleted Files

--- gameLogic.js
+++
@@ -1,7 +0,0 @@
-function lockBaseWord(){
-    if(document.getElementById("base").textContent.length() > 0){
-        document.getElementById("locker").style.visibility = "hidden";
-        document.getElementById("guess").style.visibility = "visible";
-        document.getElementById("try").style.visibility = "visible";
-    }
-}
